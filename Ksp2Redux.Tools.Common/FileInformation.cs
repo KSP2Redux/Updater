af987a67439872d4a5fec630557fcc8e7e37c4ea
Lexi <arallen3660@gmail.com>
2024-11-22 16:12:31 -0500

    Copy modifications to ScriptingAssemblies.json

--- Ksp2Redux.Tools.Common/FileInformation.cs
+++ Ksp2Redux.Tools.Common/FileInformation.cs
@@ -25,7 +25,7 @@ public static class FileInformation
         $"{Ksp2X64Data}\\level0.resS",
         $"{Ksp2X64Data}\\resources.assets",
         /* $"{Ksp2X64Data}\\RuntimeInitializeOnLoads.json", */
-        $"{Ksp2X64Data}\\ScriptingAssemblies.json",
+        /* $"{Ksp2X64Data}\\ScriptingAssemblies.json", */
         $"{Ksp2X64Data}\\sharedassets0.assets",
         $"{Ksp2X64Data}\\globalgamemanagers",
         $"{Ksp2X64Data}\\globalgamemanagers.assets",
