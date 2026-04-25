f268cddf107fb1cfac387e340b97d96e4c4b480f
Lexi <lexi@ksp2redux.org>
2026-04-25 13:44:43 -0400

    Common tool for applying patches locally used for the delta patch generation

---
+++ Ksp2Redux.Tools.PatchApplier/Program.cs
@@ -0,0 +1,31 @@
+using System.IO.Abstractions;
+using Ksp2Redux.Tools.Common;
+using Ksp2Redux.Tools.Common.Service;
+
+if (args.Length < 2)
+{
+    Console.Error.WriteLine("Usage: Ksp2Redux.Tools.PatchApplier <patchFile> <targetDir>");
+    return 1;
+}
+
+var patchFile = args[0];
+var targetDir = args[1];
+
+IFileSystem fileSystem = new FileSystem();
+IZipFileService zipFileService = new ZipFileService();
+
+using var patch = Ksp2Patch.FromFile(fileSystem, zipFileService, patchFile);
+
+var hadError = false;
+await patch.AsyncApply(
+    SystemEnvironmentProvider.Instance,
+    targetDir,
+    log: Console.WriteLine,
+    error: msg =>
+    {
+        hadError = true;
+        Console.Error.WriteLine(msg);
+    }
+);
+
+return hadError ? 1 : 0;
